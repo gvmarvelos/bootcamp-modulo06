509068f7e1d87eb0be78788a3f6da09d641aa5b3
gustavo <gvmarvelos@gmail.com>
2019-07-26 15:45:18 -0300

    chore: Screen Favorites Pronta

--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -1,7 +1,20 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { View } from 'react-native';
 import api from '../../services/api';
+
+import {
+  Conteiner,
+  Avatar,
+  Name,
+  Bio,
+  Header,
+  Stars,
+  Starred,
+  OwnerAvatar,
+  Info,
+  Title,
+  Author,
+} from './styles';
 
 export default class User extends Component {
   static navigationOptions = ({ navigation }) => ({
@@ -28,8 +41,31 @@ export default class User extends Component {
   }
 
   render() {
+    const { navigation } = this.props;
     const { stars } = this.state;
+    const user = navigation.getParam('user');
 
-    return <View />;
+    return (
+      <Conteiner>
+        <Header>
+          <Avatar source={{ uri: user.avatar }} />
+          <Name>{user.name}</Name>
+          <Bio>{user.bio}</Bio>
+        </Header>
+        <Stars
+          data={stars}
+          keyExtractor={star => String(star.id)}
+          renderItem={({ item }) => (
+            <Starred>
+              <OwnerAvatar source={{ uri: item.owner.avatar_url }} />
+              <Info>
+                <Title>{item.name}</Title>
+                <Author>{item.owner.login}</Author>
+              </Info>
+            </Starred>
+          )}
+        />
+      </Conteiner>
+    );
   }
 }
